dd8ddc9a4e423470eeffe219c3b3f8a07691b16e
jemmyasjd <jemmy33.jd@gmail.com>
2024-07-09 13:01:52 +0530

    changes in package.json

--- Backend/index.js
+++ Backend/index.js
@@ -1,47 +1,48 @@
-import { Server } from "socket.io";
-import connectDB from "./database/db.js";
-import { getDocument,updateDocument } from "./controller/document-controller.js";
-const express = require('express');
+import express from 'express';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+import connectDB from './database/db.js';
+import { getDocument, updateDocument } from './controller/document-controller.js';
+
 const app = express();
+const server = createServer(app);
 
-
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 connectDB();
 
-const io = new Server(PORT, {
+const io = new Server(server, {
     cors: {
-        origin: ["https://docsyncing.vecel.app"],
+        origin: ["https://docsyncing.vercel.app"],
         methods: ["GET", "POST"],
         allowedHeaders: ["my-custom-header"],
-        credentials: true
+        credentials: true,
     },
-    transports: ['websocket', 'polling']
+    transports: ['websocket', 'polling'],
 });
 
-io.on("connection", (socket) => {
-
-    socket.on("get-document", async documentId => {
-        const data = "";
+io.on('connection', (socket) => {
+    socket.on('get-document', async (documentId) => {
         const document = await getDocument(documentId);
         socket.join(documentId);
-        socket.emit("load-document", document.data);
+        socket.emit('load-document', document.data);
 
-        socket.on("send-changes", (delta) => {
-            socket.broadcast.to(documentId).emit("receive-changes", delta);
+        socket.on('send-changes', (delta) => {
+            socket.broadcast.to(documentId).emit('receive-changes', delta);
         });
 
         socket.on('save-document', async (data) => {
             await updateDocument(documentId, data);
         });
-
     });
-
-   
-}); 
+});
 
 app.get('/', (req, res) => {
-    res.send('Hello World')
-})
+    res.send('Hello World');
+});
 
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
